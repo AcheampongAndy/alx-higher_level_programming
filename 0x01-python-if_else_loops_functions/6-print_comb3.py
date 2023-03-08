8301453564f54a067634276cb68359f262ffcdc2
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-03-08 04:02:56 -0800

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(100):
+	if int(x / 10) != x % 10 and int(x / 10) < x % 10:
+		print("{}{}".format(int(x / 10), x % 10), end="")
+		if (x != 89):
+			print(", ", end="")
+print("")
