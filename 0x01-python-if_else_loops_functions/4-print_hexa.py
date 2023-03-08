39e6bb9ba3a350c751769f8b25eaba1026b7f176
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-03-08 11:22:42 -0800

    hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-for number in range(98):
-    print("{:d} = {:s}".format(number, hex(number)))
+def get_hex(number):
+    hexstr = '0123456789abcdef'
+    tens = number // 16
+    ones = number % 16
+    if tens > 0:
+        return hexstr[tens] + hexstr[ones]
+    else:
+        return hexstr[ones]
+
+for number in range(99):
+    print('{:d} = 0x{}'.format(number, get_hex(number)))
