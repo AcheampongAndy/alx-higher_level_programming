76adfce12e49ffc2f3d38c031e595f8857acf3cb
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-11 04:23:40 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,5 @@
 
 // A script that prints 3 lines
 
-const myLines = "C is fun\nPython is cool\nJavaScript is amazing";
+const myLines = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myLines);
