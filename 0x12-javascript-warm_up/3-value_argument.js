7d46af8c479be978f15af5d23943f3d4ee93e882
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-11 05:28:13 -0800

    value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,9 @@
 
 // Write a script that prints the first argument passed to it:
 
-let myVar = process.argv;
+const myVar = process.argv;
 console.log(
-	myVar[2] === undefined ? 'No argument':
-	myVar[2]
+  myVar[2] === undefined
+    ? 'No argument'
+    : myVar[2]
 );
