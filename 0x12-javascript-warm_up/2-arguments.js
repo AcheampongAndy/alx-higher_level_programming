d57fd2e2d3a18ef354717993dfb35bc114184985
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-11 05:26:16 -0800

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,5 +2,10 @@
 
 // Write a script that prints a message depending of the number of arguments passed:
 
-let num = process.argv.length;
-console.log(num === 2 ? 'No argument' : num === 3 ? 'Argument found' : 'Arguments found');
+const num = process.argv.length;
+console.log(num === 2
+  ? 'No argument'
+  : num === 3
+    ? 'Argument found'
+    : 'Arguments found'
+);
