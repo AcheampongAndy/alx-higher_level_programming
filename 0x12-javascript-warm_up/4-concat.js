d3c7512865455c544fa7572f2a723fb2977b4915
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-11 05:31:34 -0800

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,5 +2,5 @@
 
 // Write a script that prints two arguments passed to it, in the following format:
 
-let myVar = process.argv;
-console.log(myVar[2] + " is " + myVar[3]);
+const myVar = process.argv;
+console.log(myVar[2] + ' is ' + myVar[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-// Write a script that prints My number: <first argument converted in integer> 
+// Write a script that prints My number: <first argument converted in integer>
 // if the first argument can be converted to an integer:
 
-let myVar = process.argv;
+const myVar = process.argv;
 console.log(
-	myVar[2] === undefined || isNaN(parseInt(myVar[2])) ? "Not a number" :
-	"My number: " + parseInt(myVar[2])
+  myVar[2] === undefined || isNaN(parseInt(myVar[2]))
+    ? 'Not a number'
+    : 'My number: ' + parseInt(myVar[2])
 );
