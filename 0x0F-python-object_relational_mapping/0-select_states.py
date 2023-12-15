643c40976731b7b37ec58d45e30335ebf84fcdeb
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-15 08:34:45 -0800

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 '''
 * Write a script that lists all states from the database hbtn_0e_0_usa:
-    * Your script should take 3 arguments: mysql username, 
+    * Your script should take 3 arguments: mysql username,
     mysql password and database name (no argument validation needed)
     * You must use the module MySQLdb (import MySQLdb)
-    * Your script should connect to a MySQL server running on localhost at port 3306
+    * Your script should connect to a MySQL server
+running on localhost at port 3306
     * Results must be sorted in ascending order by states.id
     * Results must be displayed as they are in the example below
     * Your code should not be executed when imported
@@ -12,15 +13,17 @@
 import MySQLdb
 from sys import argv
 
-''' Establish a connection'''
-connection = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ = '__main__':
+    ''' Establish a connection'''
+    connection = MySQLdb.connect(host='localhost', user=argv[1],
+                                 passwd=argv[2], db=argv[3], port=3306)
 
-''' Create a cursor object to execute SQL queries'''
-cursor = connection.cursor()
+    ''' Create a cursor object to execute SQL queries'''
+    cursor = connection.cursor()
 
-cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
 
-results = cursor.fetchall()
+    results = cursor.fetchall()
 
-for row in results:
-    print(row)
+    for row in results:
+        print(row)
