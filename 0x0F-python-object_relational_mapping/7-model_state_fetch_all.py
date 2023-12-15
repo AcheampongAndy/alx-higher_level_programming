bcc10400e8cfae02c7fa409208cd406e480ac090
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-15 11:32:54 -0800

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -26,5 +26,5 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(states.id):
+    for state in session.query(State).order_by(State.id):
         print('{}: {}'.format(state.id, state.name))
