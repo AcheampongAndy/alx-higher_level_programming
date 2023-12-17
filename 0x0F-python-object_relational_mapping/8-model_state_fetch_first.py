3ccf24d7e13e2be966cd9d3c06e19ea39c6f2d39
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-12-17 15:12:25 -0800

    done

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+'''
+* Write a script that prints the first State
+    * Your script should take 3 arguments:
+    mysql username, mysql password and database name
+    * You must use the module SQLAlchemy
+    * You must import State and Base from model_state -
+    from model_state import Base, State
+    * Your script should connect to a MySQL
+    server running on localhost at port 3306
+    * The state you display must be the first in states.id
+    * You are not allowed to fetch all states
+    from the database before displaying the result
+    * The results must be displayed as they are in the example below
+    * If the table states is empty, print Nothing followed by a new line
+    * Your code should not be executed when imported
+'''
+from sys import argv
+import MySQLdb
+from model_state import Base, State
+from sqlalchemy import sessionmaker
+from sqlachemy import (create_engine)
+
+if __name__ == '__main__':
+    try:
+        username, password, database = argv[1:4]
+        engine = create_engine('mysql+mysqlconnector://{}:{}@localhost/{}'.
+                               format(username, password, database))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        first_state = session.query(State).order_by(State.id).first()
+        if first_state:
+            print('{}: {}'.format(first_state.id, first_state.name))
+        else:
+            print('Nothing')
+
+    except Exeception as e:
+        print(e)
