afcd9c2331b35336e36af35f79c27d741bce19b2
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-01-05 05:25:02 -0800

    methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | greep "Allow" | cut -d " " -f 2-
+curl -I -s "$1" | grep Allow | cut -d " " -f2-
