7856c31c820ccfd52ef7521fdf3a7e4ae34e54e9
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-01-05 09:49:50 -0800

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes
-curl -s -X PUT -L -d "user_id=98" -H "Origin: HobertonSchool" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
