b2e36a0cb64696f7ef25905e30fbcb5c4da30c44
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-01-06 08:28:25 -0800

    fetch data

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ Write a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as res:
         html = res.read()
         print('Body response')
         print(f'\t- type: {type(html)}')
