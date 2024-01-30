01d30cc596d9fe97e9202327272256464744b367
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-01-30 07:25:01 -0800

    movies

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const episode = process.argv[2];
+
+request(url + episode, function(erorr, response, body) {
+	if (error) {
+		console.log(error);
+	} else {
+		if (response.statusCode === 2000){
+			const jsonResponse = JSON.parse(body);
+			console.log(jsonResponse.title);
+		}
+	}
+});
+
